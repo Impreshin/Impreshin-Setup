94fa42cb5675c249c80f53f0e5a17692609529a1
William Stam <awstam@gmail.com>
2012-08-31 11:57:05 +0200

    fixing

--- setup-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LANG=""
-echo "updating scripts - please wait..."
-
-	git reset --hard HEAD
-	    git pull https://WilliamStam:awssmudge1@github.com/WilliamStam/Impreshin-Setup.git master
-sudo ./setup.sh
